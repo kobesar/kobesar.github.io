6956a64308321fdeb6c2d2a3b7d238bd8dbdf32c
kobesar <klobby19@gmail.com>
2023-01-16 20:25:34 -0800

    choose ebay product dash

--- dashboards/ebaydash/index.js
+++ dashboards/ebaydash/index.js
@@ -1,6 +1,6 @@
 var margin = { top: 10, right: 30, bottom: 30, left: 40 },
-  width = 460 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
+  width = 1000 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#dist")
@@ -13,56 +13,76 @@ var svg = d3.select("#dist")
 
 var avgPrice;
 
-var allGroup = ["D7200", "10-24", "24-70"]
 
-// add the options to the button
+var prod = ['d7200', 'nikkor1024', 'tamron2470']
+
 d3.select("#prod")
   .selectAll('myOptions')
-  .data(allGroup)
+  .data(prod)
   .enter()
   .append('option')
   .text(function (d) { return d; }) // text showed in the menu
   .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
-
-
-d3.csv('https://raw.githubusercontent.com/kobesar/ebaytracker/master/data/d7200.csv').then(function (data) {
+d3.csv('https://raw.githubusercontent.com/kobesar/ebaytracker/master/data/full_data.csv').then(function (data) {
   data = cleanData(data);
 
-  var x = d3.scaleLinear()
-    .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-    .range([0, width]);
+  function updateLegend(newData) {
+    svg.html("")
 
-  console.log(data)
+    var x = d3.scaleLinear()
+      .domain([0, 1000])
+      .range([0, width]);
 
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-  var histogram = d3.histogram()
-    .value(function (d) { return d.price; })   // I need to give the vector of value
-    .domain(x.domain())  // then the domain of the graphic
-    .thresholds(x.ticks(70)); // then the numbers of bins
+    var histogram = d3.histogram()
+      .value(function (d) { return d.price; })   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(70)); // then the numbers of bins
 
-  // And apply this function to data to get the bins
-  var bins = histogram(data);
+    // And apply this function to data to get the bins
+    var bins = histogram(newData);
 
-  // Y axis: scale and draw:
-  var y = d3.scaleLinear()
-    .range([height, 0]);
-  y.domain([0, d3.max(bins, function (d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  svg.append("g")
-    .call(d3.axisLeft(y));
+    // Y axis: scale and draw:
+    var y = d3.scaleLinear()
+      .range([height, 0]);
+    y.domain([0, d3.max(bins, function (d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+    svg.append("g")
+      .call(d3.axisLeft(y));
 
-  svg.selectAll("rect")
-    .data(bins)
-    .enter()
-    .append("rect")
-    .attr("x", 1)
-    .attr("transform", function (d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-    .attr("width", function (d) { return x(d.x1) - x(d.x0) - 1; })
-    .attr("height", function (d) { return height - y(d.length); })
-    .style("fill", "#69b3a2")
+    var appending = svg.selectAll('rect')
+      .data(bins).enter()
+      .append("rect")
+      .attr("x", 1)
+      .attr("transform", function (d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+      .attr("width", function (d) { return x(d.x1) - x(d.x0) - 1; })
+      .attr("height", function (d) { return height - y(d.length); })
+      .style("fill", "#69b3a2")
+
+    // remove old elements
+    appending.exit().remove();
+
+    // svg.selectAll("rect")
+    //   .data(bins)
+    //   .enter()
+    //   .append("rect")
+    //   .attr("x", 1)
+    //   .attr("transform", function (d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+    //   .attr("width", function (d) { return x(d.x1) - x(d.x0) - 1; })
+    //   .attr("height", function (d) { return height - y(d.length); })
+    //   .style("fill", "#69b3a2")
+  }
+
+  updateLegend(data);
+
+  d3.select('#prod')
+    .on('change', function () {
+      var newData = subsetData(data, d3.select(this).property('value'));
+      updateLegend(newData);
+    });
 
 })
 
@@ -77,4 +97,15 @@ function cleanData(data) {
 
   return newDat;
 
+}
+
+function subsetData(data, filter) {
+  var newDat = []
+  for (var row of data) {
+    if (row.product == filter) {
+      newDat.push(row)
+    }
+  }
+
+  return newDat;
 }
