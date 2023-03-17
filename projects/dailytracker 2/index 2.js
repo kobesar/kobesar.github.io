0ad2b1fd72b09651197edcb5e8cac1351f7c96d0
kobesar <klobby19@gmail.com>
2023-03-17 10:58:07 -0700

    new urls

---
+++ projects/dailytracker 2/index 2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function() {
+  window.addEventListener("load", init);
+
+  const SHEETS_ID = '1EXQy0GKgzoi5iMxIojUtn45ejocwWkCCMI7JnxfUlAc';
+  
+
+  function init() {
+
+  }
+
+})();
---
+++ projects/datathon/data 2.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("SPD_Crime_Data__2008-Present.csv")
+print(df.shape)
